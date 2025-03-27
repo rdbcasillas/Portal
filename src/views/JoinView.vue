f4c0afecdd2e378a0a95711af522acbbbf648f9b
vatsal <rdbcasillas11@gmail.com>
2025-03-27 16:39:53 +0530

    updated contact email

--- src/views/JoinView.vue
+++ src/views/JoinView.vue
@@ -51,7 +51,7 @@
                   d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                 />
               </svg>
-              contact@enterportal.org
+              throughportal@gmail.com
             </a>
           </div>
         </div>
